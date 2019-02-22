daca92d12c9622a891947037a915e55231ac6ae8
David Kilias <david.kilias@gmail.com>
2019-02-22 23:06:20 +0100

    elementtree ist nicht schön zum ersetzen, also auf stringbasis

--- json2xml.py
+++ json2xml.py
@@ -27,109 +27,117 @@ except Exception as e:
     print(e)
     exit(1)
 
-savePath = args.jsonDonor[0] + 'xml'
+savePath = args.jsonDonor[0] + '.xml'
 
 def isSingleDonation(dictDonor: dict) -> bool():
     return not 'Betraege' in dictDonor.keys()
 
 def createSingleDonationString(dictOrganization: dict, dictDonor: dict):
-    et = ET.XML(xmltemplates.templateXMLsingle)
-    et.find('aussteller').text = '{0}\n{1} {2}\n{3} {4}'.format(
+    template = xmltemplates.templateXMLsingle
+    template = template.replace('$aussteller', '{0}\n{1} {2}\n{3} {4}'.format(
         dictOrganization['Name'], 
         dictOrganization['Strasse'],
         dictOrganization['Hausnummer'],
         dictOrganization['PLZ'],
         dictOrganization['Stadt']
-    )
-    et.find('k3').text = dictOrganization['checkboxWegenFoerderung']
-    et.find('k4').text = dictOrganization['checkBoxEinhaltung']
-    et.find('zwecke').text = '\n'.join(dictOrganization['Zwecke'])
-    et.find('zwecke2b2').text = '\n'.join(dictOrganization['Zwecke'])
-    et.find('stnr2').text = dictOrganization['SteuerbescheidNummer']
-    et.find('finamt2').text = dictOrganization['Finanzamt']
-    et.find('datum3').text = dictOrganization['SteuerbescheidDatum']
-    et.find('k5').text = dictOrganization['checkboxEstg']
-    et.find('ort_datum').text = '{0}, {1}'.format(
+    ))
+    template = template.replace('$k3', dictOrganization['checkboxWegenFoerderung'])
+    template = template.replace('$k4', dictOrganization['checkBoxEinhaltung'])
+    template = template.replace('$zwecke1', '\n'.join(dictOrganization['Zwecke']))
+    template = template.replace('$zwecke2b2', '\n'.join(dictOrganization['Zwecke']))
+    template = template.replace('$stnr2', dictOrganization['SteuerbescheidNummer'])
+    template = template.replace('$finamt2', dictOrganization['Finanzamt'])
+    template = template.replace('$datum3', dictOrganization['SteuerbescheidDatum'])
+    template = template.replace('$k5', dictOrganization['checkboxEstg'])
+    template = template.replace('$ort_datum', '{0}, den {1}'.format(
         dictOrganization['Stadt'],
         datetime.date.today().strftime('%d.%m.%Y')
-    )
+    ))
 
-    et.find('name').text = '{0}\n{1} {2}\n{3} {4}'.format(
+    template = template.replace('$name', '{0}\n{1} {2}\n{3} {4}'.format(
         dictDonor['Name'], 
         dictDonor['Strasse'],
         dictDonor['Hausnummer'],
         dictDonor['PLZ'],
         dictDonor['Stadt']
-    )
-    et.find('wert').text = dictDonor['Betrag']
-    et.find('wert2').text = num2str(dictDonor['Betrag'])
-    et.find('datum').text = dictDonor['ZuwendungsBeginn']
+    ))
+    template = template.replace('$wert1', '{0:.2f}'.format(float(dictDonor['Betrag'])))
+
+    amountStr = '{0} Euro'.format(num2str(int(dictDonor['Betrag'])))
+    cents = int(100*(float(dictDonor['Betrag']) - int(dictDonor['Betrag'])))
+    if cents != 0:
+        amountStr = amountStr + ' und {0} Cents'.format(num2str(cents))
+
+    template = template.replace('$wert2', amountStr)
+    template = template.replace('$datum1', dictDonor['ZuwendungsBeginn'])
     
     if dictDonor['VerzichtErstattung']:
-        et.find('k1').text = 'true'
-        et.find('k2').text = 'false'
+        template = template.replace('$k1', 'true')
+        template = template.replace('$k2', 'false')
     else:
-        et.find('k1').text = 'false'
-        et.find('k2').text = 'true'
-    return et
+        template = template.replace('$k1', 'false')
+        template = template.replace('$k2', 'true')
+    return template
 
 def createMultiDonationString(dictOrganization: dict, dictDonor: dict):
-    et = ET.parse(xmltemplates.templateXMLmulti)
-    et.find('aussteller').text = '{0}\n{1} {2}\n{3} {4}'.format(
+    template = xmltemplates.templateXMLmulti
+    template = template.replace('$aussteller', '{0}\n{1} {2}\n{3} {4}'.format(
         dictOrganization['Name'], 
         dictOrganization['Strasse'],
         dictOrganization['Hausnummer'],
         dictOrganization['PLZ'],
         dictOrganization['Stadt']
-    )
-    et.find('k3').text = dictOrganization['checkboxWegenFoerderung']
-    et.find('k4').text = dictOrganization['checkBoxEinhaltung']
-    et.find('zwecke').text = '\n'.join(dictOrganization['Zwecke'])
-    et.find('zwecke2b2').text = '\n'.join(dictOrganization['Zwecke'])
-    et.find('stnr2').text = dictOrganization['SteuerbescheidNummer']
-    et.find('finamt2').text = dictOrganization['Finanzamt']
-    et.find('datum3').text = dictOrganization['SteuerbescheidDatum']
-    et.find('k5').text = dictOrganization['checkboxEstg']
-    et.find('ort_datum').text = '{0}, {1}'.format(
+    ))
+    template = template.replace('$k3', dictOrganization['checkboxWegenFoerderung'])
+    template = template.replace('$k4', dictOrganization['checkBoxEinhaltung'])
+    template = template.replace('$zwecke1', '\n'.join(dictOrganization['Zwecke']))
+    template = template.replace('$zwecke2b2', '\n'.join(dictOrganization['Zwecke']))
+    template = template.replace('$stnr2', dictOrganization['SteuerbescheidNummer'])
+    template = template.replace('$finamt2', dictOrganization['Finanzamt'])
+    template = template.replace('$datum3', dictOrganization['SteuerbescheidDatum'])
+    template = template.replace('$k5', dictOrganization['checkboxEstg'])
+    template = template.replace('$ort_datum', '{0}, {1}'.format(
         dictOrganization['Stadt'],
         datetime.date.today().strftime('%d.%m.%Y')
-    )
+    ))
 
-    et.find('name').text = '{0}\n{1} {2}\n{3} {4}'.format(
+    template = template.replace('$name', '{0}\n{1} {2}\n{3} {4}'.format(
         dictDonor['Name'], 
         dictDonor['Strasse'],
         dictDonor['Hausnummer'],
         dictDonor['PLZ'],
         dictDonor['Stadt']
-    )
-    et.find('datum').text = dictDonor['ZuwendungsBeginn']
-    et.find('datum4').text = dictDonor['ZuwendungsEnde']
+    ))
+    template = template.replace('$datum1', dictDonor['ZuwendungsBeginn'])
+    template = template.replace('$datum4', dictDonor['ZuwendungsEnde'])
 
     totalAmount = 0
-    numBetraege = 0 
+    numBetraege = 0
+    betraegeStr = ''
     for betrag in dictDonor['Betraege']:
         totalAmount = totalAmount + float(betrag['Betrag'])
         numBetraege = numBetraege + 1
-        datarow = ET.SubElement(et.find('Betraege'),'datarow')
-        ET.SubElement(datarow, 'element', attrib={'id':'ID_LINE'}).text = numBetraege
-        ET.SubElement(datarow, 'element', attrib={'id':'dat1'}).text = betrag['Datum']
-        ET.SubElement(datarow, 'element', attrib={'id':'art'}).text = betrag['Art']
-        ET.SubElement(datarow, 'element', attrib={'id':'ja_nein'}).text = betrag['VerzichtErstattung']
-        ET.SubElement(datarow, 'element', attrib={'id':'betrag1'}).text = betrag['Betrag']
+        betraegeStr = betraegeStr + '\t\t\t<datarow>\n'
+        betraegeStr = betraegeStr + '\t\t\t\t<element id="ID_LINE">{0}</element>\n'.format(numBetraege)
+        betraegeStr = betraegeStr + '\t\t\t\t<element id="dat1">{0}</element>\n'.format(betrag['Datum'])
+        betraegeStr = betraegeStr + '\t\t\t\t<element id="art">{0}</element>\n'.format(betrag['Art'])
+        betraegeStr = betraegeStr + '\t\t\t\t<element id="ja_nein">{0}</element>\n'.format(betrag['VerzichtErstattung'])
+        betraegeStr = betraegeStr + '\t\t\t\t<element id="betrag1">{0}</element>\n'.format(betrag['Betrag'])
+        betraegeStr = betraegeStr + '\t\t\t</datarow>\n'
+    template = template.replace('$betraege', betraegeStr)
+    template = template.replace('$gesamtsumme', '{0:.2f}'.format(totalAmount))
+    amountStr = '{0} Euro'.format(num2str(int(totalAmount)))
+    cents = int(100*(float(totalAmount) - int(totalAmount)))
+    if cents != 0:
+        amountStr = amountStr + ' und {0} Cents'.format(num2str(cents))
 
-    et.find('wert').text = '{0:.2f}'.format(totalAmount)
-    et.find('wert2').text = '{0} Euro und {1} Cent'.format(
-        num2str(int(totalAmount)),
-        num2str(100*(totalAmount - int(totalAmount)))
-    )
+    template = template.replace('$wert2', amountStr)
     
-    return et
+    return template
 
 if isSingleDonation(dictDonor):
-    xmlTree = createSingleDonationString(dictVerein, dictDonor)
+    foo = createSingleDonationString(dictVerein, dictDonor)
 else:
-    xmlTree = createMultiDonationString(dictVerein, dictDonor)
-
-print(savePath)
-
-xmlTree.write(open(savePath, 'w'))
+    foo = createMultiDonationString(dictVerein, dictDonor)
+with open(savePath, 'w') as outfile:
+    outfile.write(foo)
--- xmltemplates.py
+++ xmltemplates.py
@@ -1,89 +1,64 @@
 #! /usr/bin/env python3
 
-templateXMLsingle = '<?xml version="1.0" encoding="UTF-8"?> \
-<xml-data xmlns="http://www.lucom.com/ffw/xml-data-1.0.xsd">\
-	<form>catalog://Steuerformulare/gemein/034122</form>\
-	<instance>\
-		<datarow>\
-			<element id="aussteller">Musterverein\
-Musterstraße 1\
-12345 Musterstadt</element>\
-			<element id="name">Max Mustermann\
-Musterstraße 1\
-12345 Musterstadt</element>\
-			<!-- Zuwendungsbetrag -->\
-			<element id="wert">100,00</element>\
-			<element id="wert2">einhundert</element>\
-			<!-- Datum der Zuwendung -->\
-			<element id="datum">01.1.1970 00:00:00</element>\
-			<!-- Verzicht auf die Erstattung von Aufwendungen? Ja -->\
-			<element id="k1">false</element>\
-			<!-- Verzicht auf die Erstattung von Aufwendungen? Nein -->\
-			<element id="k2">true</element>\
-			<!-- Wir sind wegen Förderung... -->\
-			<element id="k3">false</element>\
-			<!-- Die Einhaltung der satzungsmäßigen Voraussetzungen -->\
-			<element id="k4">true</element>\
-			<element id="zwecke">Musterzweck 1\
-Musterzweck 2</element>\
-			<element id="stnr2">000/000/00000</element>\
-			<element id="finamt2">Musterstadt</element>\
-			<element id="datum3">01.01.1970 00:00:00</element>\
-			<element id="zwecke2b2">Musterzweck 1\
-Musterzweck 2</element>\
-			<element id="k5">false</element>\
-			<!-- Ausstellungsdatum/Ort -->\
-			<element id="ort_datum">Musterstadt, 01.01.1970</element>\
-		</datarow>\
-	</instance>\
+templateXMLsingle = '<?xml version="1.0" encoding="UTF-8"?> \n\
+<xml-data xmlns="http://www.lucom.com/ffw/xml-data-1.0.xsd">\n\
+	<form>catalog://Steuerformulare/gemein/034122</form>\n\
+	<instance>\n\
+		<datarow>\n\
+			<element id="aussteller">$aussteller</element>\n\
+			<element id="name">$name</element>\n\
+			<!-- Zuwendungsbetrag -->\n\
+			<element id="wert1">$wert1</element>\n\
+			<element id="wert2">$wert2</element>\n\
+			<!-- Datum der Zuwendung -->\n\
+			<element id="datum">$datum1</element>\n\
+			<!-- Verzicht auf die Erstattung von Aufwendungen? Ja -->\n\
+			<element id="k1">$k1</element>\n\
+			<!-- Verzicht auf die Erstattung von Aufwendungen? Nein -->\n\
+			<element id="k2">$k2</element>\n\
+			<!-- Wir sind wegen Förderung... -->\n\
+			<element id="k3">$k3</element>\n\
+			<!-- Die Einhaltung der satzungsmäßigen Voraussetzungen -->\n\
+			<element id="k4">$k4</element>\n\
+			<element id="zwecke">$zwecke1</element>\n\
+			<element id="stnr2">$stnr2</element>\n\
+			<element id="finamt2">$finamt2</element>\n\
+			<element id="datum3">$datum3</element>\n\
+			<element id="zwecke2b2">$zwecke2b2</element>\n\
+			<element id="k5">$k5</element>\n\
+			<!-- Ausstellungsdatum/Ort -->\n\
+			<element id="ort_datum">$ort_datum</element>\n\
+		</datarow>\n\
+	</instance>\n\
 </xml-data>'
 
-templateXMLmulti = '<?xml version="1.0" encoding="UTF-8"?>\
-<xml-data xmlns="http://www.lucom.com/ffw/xml-data-1.0.xsd">\
-	<form>catalog://Steuerformulare/gemein/034132</form>\
-	<instance>\
-		<datarow>\
-			<element id="aussteller">Musterverein\
-Musterstraße 1\
-12345 Musterstadt</element>\
-			<element id="name">Max Mustermann\
-Musterstraße 1\
-12345 Musterstadt</element>\
-			<element id="gesamtsumme">60.00</element>\
-			<element id="wert2">sechzig</element>\
-			<!-- Zuwendungsbeginn/Ende -->\
-			<element id="datum">01.01.1970 00:00:00</element>\
-			<element id="datum4">01.12.1970 00:00:00</element>\
-			<!-- Wir sind wegen Förderung... -->\
-			<element id="k3">false</element>\
-			<!-- Die Einhaltung der satzungsmäßigen Voraussetzungen -->\
-			<element id="k4">true</element>\
-			<element id="zwecke">Musterzweck 1\
-Musterzweck 2</element>\
-			<element id="stnr2">000/000/00000</element>\
-			<element id="finamt2">Musterstadt</element>\
-			<element id="datum3">01.06.1970 00:00:00</element>\
-			<element id="zwecke2b2">Musterzweck 1\
-Musterzweck 2</element>\
-			<element id="k5">false</element>\
-			<!-- Ausstellunsort/Datum -->\
-			<element id="ort_datum">Musterstadt, 01.01.1970</element>\
-		</datarow>\
-		<dataset id="betraege">\
-			<datarow>\
-				<element id="ID_LINE">1</element>\
-				<element id="dat1">01.01.1970 00:00:00</element>\
-				<element id="art">Mitgliedsbeitrag</element>\
-				<element id="ja_nein">nein</element>\
-				<element id="betrag1">30.00</element>\
-			</datarow>\
-			<datarow>\
-				<element id="ID_LINE">2</element>\
-				<element id="dat1">01.04.1970 00:00:00</element>\
-				<element id="art">Mitgliedsbeitrag</element>\
-				<element id="ja_nein">nein</element>\
-				<element id="betrag1">30.00</element>\
-			</datarow>\
-		</dataset>\
-	</instance>\
+templateXMLmulti = '<?xml version="1.0" encoding="UTF-8"?>\n\
+<xml-data xmlns="http://www.lucom.com/ffw/xml-data-1.0.xsd">\n\
+	<form>catalog://Steuerformulare/gemein/034132</form>\n\
+	<instance>\n\
+		<datarow>\n\
+			<element id="aussteller">$aussteller</element>\n\
+			<element id="name">$name</element>\n\
+			<element id="gesamtsumme">$gesamtsumme</element>\n\
+			<element id="wert2">$wert2</element>\n\
+			<!-- Zuwendungsbeginn/Ende -->\n\
+			<element id="datum">$datum1</element>\n\
+			<element id="datum4">$datum4</element>\n\
+			<!-- Wir sind wegen Förderung... -->\n\
+			<element id="k3">$k3</element>\n\
+			<!-- Die Einhaltung der satzungsmäßigen Voraussetzungen -->\n\
+			<element id="k4">$k4</element>\n\
+			<element id="zwecke">$zwecke1</element>\n\
+			<element id="stnr2">$stnr2</element>\n\
+			<element id="finamt2">$finamt2</element>\n\
+			<element id="datum3">$datum3</element>\n\
+			<element id="zwecke2b2">$zwecke2b2</element>\n\
+			<element id="k5">$k5</element>\n\
+			<!-- Ausstellunsort/Datum -->\n\
+			<element id="ort_datum">$ort_datum</element>\n\
+		</datarow>\n\
+		<dataset id="betraege">\n\
+$betraege\n\
+		</dataset>\n\
+	</instance>\n\
 </xml-data>'
