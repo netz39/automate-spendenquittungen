130fd80073a855857cd1e0fe2433323a9d62af17
David Kilias <david.kilias@gmail.com>
2019-03-03 14:33:51 +0100

    works for now
    creates sorted list of applicable transactions in json format

--- jameica2json.py
+++ jameica2json.py
@@ -10,15 +10,15 @@ import datetime
 
 parser = argparse.ArgumentParser(
     description='Create Spqndenquittung json files from jameica bookkeeping system')
-parser.add_argument('jameicaJson', type=str, nargs=1,
+parser.add_argument('jameicaJSON', type=str, nargs=1,
                     help='JSON file containing bookkeeping config')
-parser.add_argument('jsonListDonors', type=str, nargs=1,
+parser.add_argument('listDonorsJSON', type=str, nargs=1,
                     help='JSON file containing data on the donors (names, adresses, year)')
 
 args = parser.parse_args()
 
 try:
-    listDonors = json.load(open(os.path.abspath(args.jsonListDonors[0])))
+    listDonors = json.load(open(os.path.abspath(args.listDonorsJSON[0])))
 except Exception as e:
     print(e)
     exit(1)
@@ -38,30 +38,41 @@ con = pymysql.connect(host=jameica['dbHost'],
 
 acc = pandas.read_sql('select * from konto', con)
 trans = pandas.read_sql('select * from buchung', con)
-by = pandas.read_sql('select * from geschaeftsjahr', con)
-by_id = int(by[by.beginn.astype('str').contains(listDonors['year'])].id)
+by_id = int(pandas.read_sql(
+    'select id from geschaeftsjahr where beginn like "{}-%"'.format(listDonors["year"]), con).id)
+
 for donor in listDonors['donors']:
     # file to save info to
-    fname = listDonors['year'] + donor['name'] + '.json'
-    donor_id = getAccIdByName(donor['name'], acc)
-    if donor_id == 0:
-        print('Donor {name} not found, continuing.'.format(name=donor['name']))
+    fname = '{year}.{name}.json'.format(
+        year=listDonors['year'], name=donor['Name'])
+
+    try:
+        donor_id = int(acc[acc.name.astype('str') ==
+                           'Kreditor - ' + donor['Name']].id)
+    except Exception:
+        print('Donor {name} not found, continuing.'.format(name=donor['Name']))
         continue
     # transactions coming from 'Spenden*' or 'Mitgliedsbeiträge'
-    sourceIDs = list(acc[acc.name.str.contains('Spenden') | acc.name.str.contains('Mitgliedsbeiträge')].id) 
-    transactions = trans[trans.habenkonto_id.isin(sourceIDs) & trans.sollkonto == donor_id]
-    # sort by date
+    donationAccIDs = list(acc[acc.name.str.contains('Spenden')].id)
+    membershipAccIDs = list(acc[acc.name.str.contains('Mitgliedsbeiträge')].id)
+    tx_idx_source = trans.habenkonto_id.isin(donationAccIDs + membershipAccIDs)
+    tx_idx_donor = trans.sollkonto_id == donor_id
+    tx_idx_year = trans.geschaeftsjahr_id == by_id
+    tx_idx_valid = tx_idx_source & tx_idx_donor & tx_idx_year
+    validTxs = trans[tx_idx_valid]
+    
     donorDict = donor
-    for transaction in transactions:
-        tr = dict()
-        tr['Datum'] = transaction.datum
-        tr['Art'] = "Geldzuwendung" # Mitgliedsbeitrag
-        tr['VerzichtErstattung']="nein"
-        tr['Betrag'] = transaction.betrag
-        donorDict['Betraege'].append(tr)
-
-def getAccIdByName(name: str, acc: pandas.DataFrame):
-    try:
-        return int(acc[acc.name.astype('str') == 'Kreditor - ' + name].id)
-    except Exception:
-        return 0
+    transactionList = []
+    for idx, tx in validTxs.iterrows():
+        txDict = dict()
+        txDict['Datum'] = tx.datum.isoformat()
+        if tx.habenkonto_id in membershipAccIDs:
+            txDict['Art'] = "Mitgliedsbeitrag"
+        else:
+            txDict['Art'] = "Geldzuwendung"
+        txDict['VerzichtErstattung'] = "nein"
+        txDict['Betrag'] = tx.betrag
+        transactionList.append(txDict)
+    donorDict['Betraege'] = sorted(transactionList, key=lambda tx: tx['Datum'])
+    with open(fname, 'w') as fp:
+        json.dump(donorDict, fp, indent=4)
