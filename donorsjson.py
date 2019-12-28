816319e10e76f95cf7b99d32fa441acc7501b4a5
David <david.kilias@gmail.com>
2019-12-28 14:18:17 +0100

    script to create donors.json from textfile

---
+++ donorsjson.py
@@ -0,0 +1,15 @@
+import sys
+import json
+
+donors = dict()
+donors['year'] = sys.argv[1]
+donors['donors'] = []
+
+lines = sys.stdin.readlines()
+for i in range(0, len(lines), 4):
+    d = dict()
+    d['name'] = lines[i]
+    d['address'] = ' '.join(lines[i+1:i+3])
+    donors['donors'].append(d)
+
+json.dump(donors, sys.stdout, indent=4, ensure_ascii=False)
